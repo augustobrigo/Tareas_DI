70968eafc049ab2fc62c4e89d117880ed9fa6c75
augustobrigo <profeaugustobriga@gmail.com>
2023-09-25 22:01:39 +0200

    conectar BD

--- Conectar_BD/Conectar_BD/Form1.cs
+++ Conectar_BD/Conectar_BD/Form1.cs
@@ -6,11 +6,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace Conectar_BD
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conexion;
+        MySqlCommand comando;
+        MySqlDataReader adaptador;
         public Form1()
         {
             InitializeComponent();
@@ -18,6 +23,18 @@ namespace Conectar_BD
 
         private void Form1_Load(object sender, EventArgs e)
         {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conexion = new MySqlConnection();
+
+            conexion.ConnectionString = "Server=localhost;Port=3306;DataBase=dam2023;user=root;Pwd='';old guids=true;";
+            conexion.Open();
+            String cadenaSql = "show tables";
+            comando = new MySqlCommand(cadenaSql, conexion);
+            
 
         }
     }
