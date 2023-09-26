6adf209272edb77cfc7cc9a8dbd508f6533a39d2
augustobrigo <profeaugustobriga@gmail.com>
2023-09-26 18:32:31 +0200

    Avanzar_Retroceder_Colores

---
+++ Avanzar_Retroceder_Colores/Colorines/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Colorines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button51 = new System.Windows.Forms.Button();
+            this.button52 = new System.Windows.Forms.Button();
+            this.button53 = new System.Windows.Forms.Button();
+            this.button54 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(64, 45);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(457, 393);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button51
+            // 
+            this.button51.Location = new System.Drawing.Point(578, 45);
+            this.button51.Name = "button51";
+            this.button51.Size = new System.Drawing.Size(87, 23);
+            this.button51.TabIndex = 1;
+            this.button51.Text = "Avanza 1";
+            this.button51.UseVisualStyleBackColor = true;
+            this.button51.Click += new System.EventHandler(this.button51_Click);
+            // 
+            // button52
+            // 
+            this.button52.Location = new System.Drawing.Point(578, 90);
+            this.button52.Name = "button52";
+            this.button52.Size = new System.Drawing.Size(87, 23);
+            this.button52.TabIndex = 2;
+            this.button52.Text = "Back 1";
+            this.button52.UseVisualStyleBackColor = true;
+            this.button52.Click += new System.EventHandler(this.button52_Click);
+            // 
+            // button53
+            // 
+            this.button53.Location = new System.Drawing.Point(578, 144);
+            this.button53.Name = "button53";
+            this.button53.Size = new System.Drawing.Size(87, 23);
+            this.button53.TabIndex = 3;
+            this.button53.Text = "AvanzaTodo";
+            this.button53.UseVisualStyleBackColor = true;
+            this.button53.Click += new System.EventHandler(this.button53_Click);
+            // 
+            // button54
+            // 
+            this.button54.Location = new System.Drawing.Point(578, 208);
+            this.button54.Name = "button54";
+            this.button54.Size = new System.Drawing.Size(87, 23);
+            this.button54.TabIndex = 4;
+            this.button54.Text = "Back Todo";
+            this.button54.UseVisualStyleBackColor = true;
+            this.button54.Click += new System.EventHandler(this.button54_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.button54);
+            this.Controls.Add(this.button53);
+            this.Controls.Add(this.button52);
+            this.Controls.Add(this.button51);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Avanzar Colores";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button51;
+        private System.Windows.Forms.Button button52;
+        private System.Windows.Forms.Button button53;
+        private System.Windows.Forms.Button button54;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+    }
+}
+
---
+++ Avanzar_Retroceder_Colores/Colorines/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Colorines
+{
+    public partial class Form1 : Form
+    {
+        int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Button b = new Button();
+                b.BackColor = Color.Green;
+                flowLayoutPanel1.Controls.Add(b);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Cuando ya pasa el rojo vuelve a verde. Todo lo que no sea posición inicial. Desactoiva
+            if (contador != 0)
+            {
+                flowLayoutPanel1.Controls[contador - 1].BackColor = Color.Green;
+            }
+            //empezar por aquí activando el rojo desde principio hasta el fin
+            if (contador < flowLayoutPanel1.Controls.Count)
+            {
+                flowLayoutPanel1.Controls[contador].BackColor = Color.Red;
+                contador++;
+            }
+            else
+            {
+                contador = 0;
+            }
+        }
+
+        private void button51_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            resetearBotones();
+
+            contador = 0;
+            timer1.Start();
+
+        }
+
+        private void button52_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            resetearBotones();
+
+            contador = flowLayoutPanel1.Controls.Count - 1;
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //retroceso
+            if (contador == 0)
+            {
+                flowLayoutPanel1.Controls[contador].BackColor = Color.Green;
+            }
+            //si llega a la última posición
+            if (contador == flowLayoutPanel1.Controls.Count - 1)
+            {
+                flowLayoutPanel1.Controls[contador].BackColor = Color.Red;
+            }
+            //mientras no llegue a la última posición
+            if (contador != 0)
+            {
+                //coloca el rojo
+                flowLayoutPanel1.Controls[contador - 1].BackColor = Color.Red;
+                //retorna al verde
+                flowLayoutPanel1.Controls[contador].BackColor = Color.Green;
+                contador--;
+            }
+            else
+            {
+                //vuelve al final
+                contador = flowLayoutPanel1.Controls.Count - 1;
+            }
+        }
+
+        private void button53_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            resetearBotones();
+
+            contador = 0;
+            timer3.Start();
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            //arrastra todos los colores si está en verde pasa a rojo. Siempre y cuando no llegue al final
+            if (contador < flowLayoutPanel1.Controls.Count)
+            {
+                //si está verde pasa a rojo
+                if (flowLayoutPanel1.Controls[contador].BackColor == Color.Green)
+                {
+                    flowLayoutPanel1.Controls[contador].BackColor = Color.Red;
+                }
+                //si está en rojo pasa a verde
+                else if (flowLayoutPanel1.Controls[contador].BackColor == Color.Red)
+                {
+                    flowLayoutPanel1.Controls[contador].BackColor = Color.Green;
+                }
+                //avanzar
+                contador++;
+            }
+            else
+            {
+                //resetear a 0 cuando el cursor llega hasta el final
+                contador = 0;
+            }
+
+        }
+
+        private void button54_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            resetearBotones();
+
+            contador = flowLayoutPanel1.Controls.Count - 1;
+            timer4.Start();
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            //si contador menor que 0 se va a la última posición del panel
+            if (contador < 0)
+            {
+                contador = flowLayoutPanel1.Controls.Count - 1;
+            }
+            else
+            {
+                //si tiene botón rojo se pasa a verde
+                if (flowLayoutPanel1.Controls[contador].BackColor == Color.Red)
+                {
+                    flowLayoutPanel1.Controls[contador].BackColor = Color.Green;
+                }//si tiene color verde pasa a rojo
+                else if (flowLayoutPanel1.Controls[contador].BackColor == Color.Green)
+                {
+                    flowLayoutPanel1.Controls[contador].BackColor = Color.Red;
+                }
+                //retrocede
+                contador--;
+            }
+        }
+        private void resetearBotones()
+        {
+            //recorrer todo el panel de botones y ponerlos a verde
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+            {
+                flowLayoutPanel1.Controls[i].BackColor = Color.Green;
+            }
+        }
+    }
+}
